2b917fdb11f6400b64dda8d21fc27355ee8978e2
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-05 01:52:42 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+void merge(int arr[], int left, int middle, int right) {
+    int i, j, k;
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+    int L[n1], R[n2];
+ 
+
+    printf("Given array is \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    mergeSort(arr, 0, n - 1);
+    printf("Sorted array is \n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
