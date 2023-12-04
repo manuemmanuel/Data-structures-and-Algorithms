c4d85e819d564ee36b397a37bcdaa7d78807a9f6
Manu Emmanuel <manuemmanuel2026@cs.sjcetpalai.ac.in>
2023-12-05 01:54:19 +0530

    Update mergesort.c

--- mergesort.c
+++ mergesort.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
-void merge(int arr[], int left, int middle, int right) {
-    int i, j, k;
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
-    int L[n1], R[n2];
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[middle + 1 + j];
+
